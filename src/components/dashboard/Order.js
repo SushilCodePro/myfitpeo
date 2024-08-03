e3b547cfb3a5b9e534bac4db67384af0bc6ecdc6
Sushil Gupta <sushil.codepro@gmail.com>
2024-08-03 23:57:00 +0530

    table improve

--- src/components/dashboard/Order.js
+++ src/components/dashboard/Order.js
@@ -13,22 +13,22 @@ function Order() {
     return (
 
         <div className="w-full md:flex-2 h-72 bg-gray-700 rounded">
-            <table className="min-w-full text-white">
+            <table className="min-w-full text-white text-sm md:text-base">
                 <thead>
                     <tr>
-                        <th className="py-2 px-4 border-b">ID</th>
-                        <th className="py-2 px-4 border-b">Name</th>
-                        <th className="py-2 px-4 border-b">Email</th>
-                        <th className="py-2 px-4 border-b">Age</th>
+                        <th className="py-2 px-2 md:px-4 border-b">ID</th>
+                        <th className="py-2 px-2 md:px-4 border-b">Name</th>
+                        <th className="py-2 px-2 md:px-4 border-b">Email</th>
+                        <th className="py-2 px-2 md:px-4 border-b">Age</th>
                     </tr>
                 </thead>
                 <tbody>
                     {data.map((item) => (
                         <tr key={item.id}>
-                            <td className="py-2 px-4 border-b text-center ">{item.id}</td>
-                            <td className="py-2 px-4 border-b text-center">{item.name}</td>
-                            <td className="py-2 px-4 border-b text-center">{item.email}</td>
-                            <td className="py-2 px-4 border-b text-center">{item.age}</td>
+                            <td className="py-1 px-2 md:py-2 md:px-4 border-b text-center">{item.id}</td>
+                            <td className="py-1 px-2 md:py-2 md:px-4 border-b text-center">{item.name}</td>
+                            <td className="py-1 px-2 md:py-2 md:px-4 border-b text-center">{item.email}</td>
+                            <td className="py-1 px-2 md:py-2 md:px-4 border-b text-center">{item.age}</td>
                         </tr>
                     ))}
                 </tbody>

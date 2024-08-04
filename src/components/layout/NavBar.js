4eda07cf708a0c938793c53f7e2fef1cd30fe1e6
Sushil Gupta <sushil.codepro@gmail.com>
2024-08-04 15:19:52 +0530

    logo shift

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -12,7 +12,7 @@ function NavBar() {
     }
     return (
         <div className="bg-gray-700 h-auto flex items-center flex-col sm:flex-row space-y-1 sm:space-y-0  ">
-            <div className='w-full sm:flex-1 flex items-center ml-1 justify-around sm:justify-normal'>
+            <div className='w-full sm:flex-1 flex items-center p-1 justify-between sm:justify-normal'>
                 <div className='icon'>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-10 text-blue-500 ml-1">
                         <path fillRule="evenodd" d="M11.99 2.243a4.49 4.49 0 0 0-3.398 1.55 4.49 4.49 0 0 0-3.497 1.306 4.491 4.491 0 0 0-1.307 3.498 4.491 4.491 0 0 0-1.548 3.397c0 1.357.6 2.573 1.548 3.397a4.491 4.491 0 0 0 1.307 3.498 4.49 4.49 0 0 0 3.498 1.307 4.49 4.49 0 0 0 3.397 1.549 4.49 4.49 0 0 0 3.397-1.549 4.49 4.49 0 0 0 3.497-1.307 4.491 4.491 0 0 0 1.306-3.497 4.491 4.491 0 0 0 1.55-3.398c0-1.357-.601-2.573-1.549-3.397a4.491 4.491 0 0 0-1.307-3.498 4.49 4.49 0 0 0-3.498-1.307 4.49 4.49 0 0 0-3.396-1.549Zm3.53 7.28a.75.75 0 0 0-1.06-1.06l-6 6a.75.75 0 1 0 1.06 1.06l6-6Zm-5.78-.905a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25Zm4.5 4.5a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25Z" clipRule="evenodd" />
